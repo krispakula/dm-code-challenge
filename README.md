6e5154256b3b0db841288a3c94eb1bccba004025
Krzysztof PAKULA <46619844+krispakula@users.noreply.github.com>
2020-01-14 11:07:51 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# dm-code-challenge
-Code Challenge
+
+### dosing.py README
+
+---
+
+This project provides the basic instructions to run the dosing.py script.
+
+#### Usage
+
+1. configure virtual environment
+   >virtualenv env
+
+2. Install packages
+   >pip install -r requirements.txt
+
+3. run dosing.py script with optional parameters viscode, svdose, ecsdstxt and results
+   >python dosing.py [--viscode=VISCODE] [--svdose=SVDOSE] [--ecsdstxt=ECSDSTXT] [--results=RESULTS]
+
+   ###### Example:
+
+   >python dosing.py --viscode='w02' --svdose='Y' --ecsdstxt='180.0' --results=="results.csv"
+   >python dosing.py --help
---
+++ dosing.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import pytest
+import params 
+from optparse import OptionParser
+
+def read_csv_files():
+   df_ec = pd.read_csv("t2_ec 20190619.csv")
+   df_registry = pd.read_csv("t2_registry 20190619.csv")
+   df_registry = df_registry[ df_registry.SVPERF == 'Y' ]
+   df_registry = df_registry[ df_registry.VISCODE != 'bl' ]
+   return df_ec, df_registry
+
+def draw_pie(df_ec, df_registry):
+   fig = go.Figure(go.Pie(
+      name = "",
+      labels = df_registry.VISCODE,
+      hovertemplate = "Viscode: <B>%{label}</B><br><B>Count: %{value} (%{percent}) </B>",
+      title = dict(text="Viscodes from Registry", font=dict(size=20), position="top left") 
+   ))
+   fig.show()
+   return
+
+# Read CSV files
+def csv_report():
+   df_ec = pd.read_csv("t2_ec 20190619.csv")
+   df_registry = pd.read_csv("t2_registry 20190619.csv")
+   return df_ec, df_registry
+
+# Merge dataframes on RID and VISCODE
+def merge_dataframes(df_ec, df_registry):
+   df_merge = pd.merge(df_registry, df_ec, on = ['RID','VISCODE'], how='outer', indicator=True) 
+   return(df_merge)
+
+# Filter records
+def filter_records(df, options):
+   df_filtered = df[ df_merge.VISCODE == options.viscode ]
+   df_filtered = df_filtered[ df_filtered.SVDOSE == options.svdose ]
+   df_filtered = df_filtered[ df_filtered.ECSDSTXT != options.ecsdstxt ]
+   return df_filtered
+
+# Create CSV file 
+def create_csv_file(df, options):
+   df_csv = df[['ID_x','RID','USERID_x','VISCODE','SVDOSE','ECSDSTXT']]
+   print(df_csv) 
+   csv = df_csv.to_csv(options.results, index=False, header=False) 
+   print(csv) 
+
+if __name__ == "__main__":
+
+   parser = OptionParser()
+   parser.add_option('', '--viscode',  dest='viscode',  default='w02', help='Viscode parameter') 
+   parser.add_option('', '--svdose',   dest='svdose',   default='Y', help='Svdose parameter') 
+   parser.add_option('', '--ecsdstxt', dest='ecsdstxt', default='180', help='Ecsdstxt parameter') 
+   parser.add_option('', '--results',  dest='results',  default='results.csv', help='results directory parameter') 
+   options = parser.parse_args()[0]
+
+   df_ec,df_registry = read_csv_files()
+   draw_pie(df_ec, df_registry)
+   df_merge = merge_dataframes(df_ec, df_registry)
+   df_filtered = filter_records(df_merge, options)
+   create_csv_file(df_filtered, options) 
+
