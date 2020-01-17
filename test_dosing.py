0c422f53f4494c2c6ea2fea1e5f93387a23b9e2d
Krzysztof PAKULA <46619844+krispakula@users.noreply.github.com>
2020-01-17 15:55:06 -0800

    Add files via upload

--- test_dosing.py
+++ test_dosing.py
@@ -13,12 +13,14 @@ class Testing(unittest.TestCase):
 
     # test filter_records function
     def test_filter_records(self):
-        assert dosing.filter_records(df_ec, 'RID' 'w04', "Y", "Y" ).ENTRY[0] == 4
-        assert dosing.filter_records(df_registry, 'RID', 'w04', "Y", "Y").USERID[0] == 'BBAGGINS_UCSD_EDU_2'
+        df = dosing.merge_dataframes(df_registry, df_ec, 'RID', 'VISCODE')
+
+        assert dosing.filter_records(df,  'w02', "Y", "Y" ).SVSTDT[7] == '8/9/18'
+        assert dosing.filter_records(df, 'w02', "Y" , "Y").USERID_x[7] == 'BBAGGINS_UCSD_EDU_2'
 
     def test_merge_dataframes(self):
-        assert dosing.merge_dataframes(df_registry, df_ec, 'RID', 'VISCODE').SVSTDT[0] == '8/9/18'
-        assert dosing.merge_dataframes(df_ec, df_registry,'RID', 'VISCODE').SVRESCRN[0] == '-4'
+        assert dosing.merge_dataframes(df_registry, df_ec, 'RID', 'VISCODE').SVSTDT[7] == '8/9/18'
+        assert dosing.merge_dataframes(df_ec, df_registry,'RID', 'VISCODE').SVRESCRN[7] == '-4'
 
 if __name__ == '__main__':
 
