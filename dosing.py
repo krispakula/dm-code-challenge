05fca3637b091d5aba38a8feba79335d67705097
Krzysztof PAKULA <46619844+krispakula@users.noreply.github.com>
2020-01-17 14:37:07 -0800

    Add files via upload

--- dosing.py
+++ dosing.py
@@ -5,12 +5,10 @@ import pytest
 import params 
 from optparse import OptionParser
 
-def read_csv_files():
-   df_ec = pd.read_csv("t2_ec 20190619.csv")
-   df_registry = pd.read_csv("t2_registry 20190619.csv")
-   df_registry = df_registry[ df_registry.SVPERF == 'Y' ]
-   df_registry = df_registry[ df_registry.VISCODE != 'bl' ]
-   return df_ec, df_registry
+df_ec = pd.read_csv("t2_ec 20190619.csv")
+df_registry = pd.read_csv("t2_registry 20190619.csv")
+df_registry = df_registry[ df_registry.SVPERF == 'Y' ]
+df_registry = df_registry[ df_registry.VISCODE != 'bl' ]
 
 def draw_pie(df_ec, df_registry):
    fig = go.Figure(go.Pie(
@@ -29,15 +27,16 @@ def csv_report():
    return df_ec, df_registry
 
 # Merge dataframes on RID and VISCODE
-def merge_dataframes(df_ec, df_registry):
+def merge_dataframes(df_ec, df_registry, rid='RID', viscode='VISCODE'):
    df_merge = pd.merge(df_registry, df_ec, on = ['RID','VISCODE'], how='outer', indicator=True) 
-   return(df_merge)
+   return df_merge
 
 # Filter records
-def filter_records(df, options):
-   df_filtered = df[ df_merge.VISCODE == options.viscode ]
-   df_filtered = df_filtered[ df_filtered.SVDOSE == options.svdose ]
-   df_filtered = df_filtered[ df_filtered.ECSDSTXT != options.ecsdstxt ]
+def filter_records(df, viscode, svdose, ecsdstxt):
+   df_filtered = df[ df_merge.VISCODE == viscode ]
+   df_filtered = df_filtered[ df_filtered.SVDOSE == svdose ]
+   df_filtered = df_filtered[ df_filtered.ECSDSTXT != ecsdstxt ]
+   print(df_filtered)
    return df_filtered
 
 # Create CSV file 
@@ -51,14 +50,14 @@ if __name__ == "__main__":
 
    parser = OptionParser()
    parser.add_option('', '--viscode',  dest='viscode',  default='w02', help='Viscode parameter') 
+   parser.add_option('', '--rid',      dest='rid',      default='RID', help='Rid parameter') 
    parser.add_option('', '--svdose',   dest='svdose',   default='Y', help='Svdose parameter') 
    parser.add_option('', '--ecsdstxt', dest='ecsdstxt', default='180', help='Ecsdstxt parameter') 
    parser.add_option('', '--results',  dest='results',  default='results.csv', help='results directory parameter') 
    options = parser.parse_args()[0]
 
-   df_ec,df_registry = read_csv_files()
    draw_pie(df_ec, df_registry)
-   df_merge = merge_dataframes(df_ec, df_registry)
-   df_filtered = filter_records(df_merge, options)
+   df_merge = merge_dataframes(df_ec, df_registry, options.rid, options.viscode)
+   df_filtered = filter_records(df_merge,  options.viscode, options.svdose, options.ecsdstxt)
    create_csv_file(df_filtered, options) 
 
